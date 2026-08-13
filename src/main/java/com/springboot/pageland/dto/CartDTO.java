2af327573c1f014398b7a0b2a86f0424f1846744
pageland12 <pageland12@gmail.com>
2026-08-13 18:00:02 +0900

    Update CartDTO.java

--- src/main/java/com/springboot/pageland/dto/CartDTO.java
+++ src/main/java/com/springboot/pageland/dto/CartDTO.java
@@ -4,11 +4,15 @@ import lombok.Data;
 
 @Data
 public class CartDTO {
-	private	int		cno;
-	private	String	ctype;
-	private	String	cstock;
-	private	int		mno;
+	private	int			cno;
+	private	String		ctype;
+	private	int		cstock;
+	private	int			mno;
 	private	Integer	 	bno;
-	private String 	bname;
-	private String 	bprice;
+	private String 		bname;
+	private String 		bprice;
+	private	Integer		pno;
+	private String		pname;
+	private String		pprice;
+	private String		ptype;
 }
